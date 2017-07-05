3e7ca847022ff6d2e31b3fe1248d7686230dc5e2
Gantcho Kojuharov <gantcho.kojuharov@ictu.nl>
2017-07-05 12:32:20 +0200

    check if version is valid when releasing

--- release.sh
+++ release.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 
 VERSION=$1
-echo Releasing BigBoat version $VERSION
-npm version -m "Bumped version to %s for release." $VERSION
-git push
-git push --tags
+if semver $VERSION; then
+  echo Releasing BigBoat version $VERSION
+  npm version -m "Bumped version to %s for release." $VERSION
+  git push
+  git push --tags
+else
+  echo $VERSION is not a valid semantic version.
+fi
