b0bf310d3ee62a04c5afee10f4b0659191e4d4fd
Gantcho Kojuharov <gantcho.kojuharov@ictu.nl>
2017-07-28 15:23:00 +0200

    point to the local semver for macos compat

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 VERSION=$1
-if semver $VERSION; then
+if node_modules/.bin/semver $VERSION; then
   echo Releasing BigBoat version $VERSION
   npm version -m "Bumped version to %s for release." $VERSION
   git push
